182145c0dd61cb7a30e75d63bee92f730051bfb3
Marquetta <73968023+quettamar@users.noreply.github.com>
2022-01-14 15:19:40 -0600

    finished pypoll and need to calculate average change for pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,13 +11,11 @@ with open(budget_data_path, "r") as budget_csv:
     #look at the csv file's budget data and setting it up as a list
     data = list(csv.reader(budget_csv))
     #this is skipping the header
-    data = data[1:]
-
-print(data)        
+    data = data[1:]      
             
 total_months = len(data)    
 total_amount = 0
-profit_loss_column = []   
+profit_loss_column = []  
 profit_change = 0
 
 for row in data:
@@ -25,10 +23,8 @@ for row in data:
     #total_amount = total_amount + int[(row[1])]
     total_amount += int(row[1])
     profit_loss_column.append(int(row[1]))
-    
-
-#print(total_amount)   
-print(total_months)
+    #profit_change = profit_change - int[(row[1])]
+    #profit_loss_column.append(int(row[1]))
 
 #the greatest increase in profits (date and amount) over the entire period
 greatest_increase = max(profit_loss_column)
@@ -40,32 +36,27 @@ date_decrease = data[profit_loss_column.index(greatest_decrease)][0]
 
 #calculating the average change
 average_loss = total_amount / total_months
-#print(average_loss)
-#print(date_decrease)
-#print(greatest_decrease)
-
-#print(greatest_increase)
-#print(date_increase)
 
 #create a loop that will calculate the amount of change between each month
 
-#create a txt file with the results
-#f = open("budget_results.txt", "w")
-results_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "..", "PyBank", "Analysis")
+#come back later to figure out how to change directories to the analysis folder
+#results_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "..", "PyBank", "Analysis")
 f = open("budget_results.txt", "w")
 
-f.write(f"Financial Analysis\n----------------------------------\n")
+f.write(f"Financial Analysis\n")
+f.write("----------------------\n")
 f.write(f"Your profit is: ${total_amount}\n")
 f.write(f"Total Months {total_months}\n")
 #f.write(f"Average  Change: ${}\n")
-f.write(f"Greatest Increase in Profits: (${date_increase})\n")
-f.write(f"Greatest Decrease in Profits: (${date_decrease})\n")
+f.write(f"Greatest Increase in Profits: {date_increase}\n")
+f.write(f"Greatest Decrease in Profits: {date_decrease}\n")
 
-print(f"\nFinancial Analysis\n----------------------------------\n")
-print(f"Your profit is: ${total_amount}\n")
+print(f"Financial Analysis")
+print("-"*25)
+print(f"Your profit is: ${total_amount}")
 print(f"Total Months: 86 months")
 #print(f"Average  Change: ${}")
-print(f"Greatest Increase in Profits was in {date_increase} for (${greatest_increase}.)")
-print(f"Greatest Decrease in Profits was in {date_decrease} for (${greatest_decrease}.)")
+print(f"Greatest Increase in Profits was in {date_increase} for ${greatest_increase}.")
+print(f"Greatest Decrease in Profits was in {date_decrease} for ${greatest_decrease}.")
 
 f.close
