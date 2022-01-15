98ae257e339b63c1be6105e586ea6f56e9aa2cab
Marquetta <73968023+quettamar@users.noreply.github.com>
2022-01-14 19:32:00 -0600

    updated code to figure out greatest increase and decrease but need to figure out why average change is positive instead of negative

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+from audioop import avg
 import os
 import csv
 import pathlib
@@ -16,47 +17,49 @@ with open(budget_data_path, "r") as budget_csv:
 total_months = len(data)    
 total_amount = 0
 profit_loss_column = []  
-profit_change = 0
+profit_change = []
+profit_loss = 0
 
 for row in data:
     #this is adding up all the volumes in the profit colum
     #total_amount = total_amount + int[(row[1])]
     total_amount += int(row[1])
     profit_loss_column.append(int(row[1]))
-    #profit_change = profit_change - int[(row[1])]
-    #profit_loss_column.append(int(row[1]))
+
+    profit_change.append(int(row[1])-profit_loss)
+    profit_loss = int(row[1])
+    
+    
 
 #the greatest increase in profits (date and amount) over the entire period
-greatest_increase = max(profit_loss_column)
-date_increase = data[profit_loss_column.index(greatest_increase)][0]
+greatest_increase = max(profit_change)
+date_increase = data[profit_change.index(greatest_increase)][0]
 
 #the greatest decrease in profits (date and amount) over the entire period
-greatest_decrease = min(profit_loss_column)
-date_decrease = data[profit_loss_column.index(greatest_decrease)][0]
+greatest_decrease = min(profit_change)
+date_decrease = data[profit_change.index(greatest_decrease)][0]
 
 #calculating the average change
-average_loss = total_amount / total_months
+total_change = sum(profit_change)
+#come back and figure out why you're getting a positive number
+profit_average_change = total_change / total_months
 
-#create a loop that will calculate the amount of change between each month
-
-#come back later to figure out how to change directories to the analysis folder
-#results_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "..", "PyBank", "Analysis")
-#f = open("budget_results.txt", "w")
+#create a txt file with the budget analysis results
 f = open(r"C:\\Users\marqu\OneDrive\Desktop\Repositories\Python-Challenge\PyBank\Analysis\budget_results.txt", "w")
 
 f.write(f"Financial Analysis\n")
 f.write("----------------------\n")
 f.write(f"Your profit is: ${total_amount}\n")
 f.write(f"Total Months {total_months}\n")
-#f.write(f"Average  Change: ${}\n")
-f.write(f"Greatest Increase in Profits: {date_increase}\n")
-f.write(f"Greatest Decrease in Profits: {date_decrease}\n")
+f.write(f"Average  Change: ${profit_average_change}\n")
+f.write(f"Greatest Increase in Profits: {date_increase} (${greatest_increase})\n")
+f.write(f"Greatest Decrease in Profits: {date_decrease} (${greatest_decrease})\n")
 
 print(f"Financial Analysis")
 print("-"*25)
 print(f"Your profit is: ${total_amount}")
 print(f"Total Months: 86 months")
-#print(f"Average  Change: ${}")
+print(f"Average  Change: ${profit_average_change}")
 print(f"Greatest Increase in Profits was in {date_increase} for ${greatest_increase}.")
 print(f"Greatest Decrease in Profits was in {date_decrease} for ${greatest_decrease}.")
 
